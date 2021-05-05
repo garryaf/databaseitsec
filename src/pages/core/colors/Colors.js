1bdbecab6177bd46a6ae1e61a4dc8e0c02f16de3
yurykurylovich <yury.kurylovich@gmail.com>
2021-05-05 17:59:11 +0300

    Styled typography colors widget

--- src/pages/core/colors/Colors.js
+++ src/pages/core/colors/Colors.js
@@ -43,14 +43,40 @@ export default function Colors() {
             </Col>
           </Row>
           <Row>
-            <Col xs={12} lg={6}>
+            <Col xs={12} lg={6} className="mb-4 mb-lg-0">
               <Widget>
                 <div className={s.tableTitle}>
                   <div className="headline-2">Typography Colors</div>
                 </div>
-                <div className={s.widgetContentBlock}>
-                  Content Block
-                </div>
+                <Row className="widget-p-md pt-2">
+                  <p className="px-3 mb-4">Convey meaning through color with a handful of color utility classes.
+                    Includes support for styling links with hover states, too. Use <code>text-*</code> class to fill text.</p>
+                  <Col xs={12} sm={6} className="mb-3 mb-sm-0">
+                    <div className="mb-3">
+                      <p className="headline-1 mb-0 text-primary">Headline 1</p>
+                    </div>
+                    <div className="mb-3">
+                      <p className="headline-2 mb-0 text-secondary-red">Headline 2</p>
+                    </div>
+                    <div>
+                      <p className="headline-3 mb-0 text-success">Headline 2</p>
+                    </div>
+                  </Col>
+                  <Col xs={12} sm={6}>
+                    <div className="mb-3">
+                      <p className="body-1 mb-0 text-info">Body 1</p>
+                    </div>
+                    <div className="mb-3">
+                      <p className="body-2 mb-0 text-warning">Body 2</p>
+                    </div>
+                    <div className="mb-3">
+                      <p className="body-3 mb-0 text-secondary-cyan">Body 3</p>
+                    </div>
+                    <div>
+                      <p className="label mb-0 text-danger">Label</p>
+                    </div>
+                  </Col>
+                </Row>
               </Widget>
             </Col>
             <Col xs={12} lg={6}>
