a65f6be50539e940487ce3935d43da8b9b6c32fa
yurykurylovich <yury.kurylovich@gmail.com>
2021-05-12 16:08:34 +0300

    Configured responsive layout

--- src/pages/uielements/badges/Badges.js
+++ src/pages/uielements/badges/Badges.js
@@ -1,7 +1,43 @@
 import React from "react";
+import {
+  Col,
+  Row,
+  Badge
+} from "reactstrap";
+import Widget from "../../../components/Widget/Widget.js";
+import s from "./Badges.module.scss";
 
 export default function Badges() {
   return (
-    <div>Badges page</div>
+    <div>
+      <Row>
+        <Col>
+          <Row className="gutter mb-4">
+            <Col xs={12} md={6}>
+              <Widget className="widget-p-md"><div> Widget </div></Widget>
+            </Col>
+            <Col xs={12} md={6} className="mt-4 mt-md-0">
+              <Widget className="widget-p-md"><div> Widget </div></Widget>
+            </Col>
+          </Row>
+          <Row className="gutter mb-4">
+            <Col xs={12} md={6}>
+              <Widget className="widget-p-md"><div> Widget </div></Widget>
+            </Col>
+            <Col xs={12} md={6} className="mt-4 mt-md-0">
+              <Widget className="widget-p-md"><div> Widget </div></Widget>
+            </Col>
+          </Row>
+          <Row className="gutter">
+            <Col xs={12} md={6}>
+              <Widget className="widget-p-md"><div> Widget </div></Widget>
+            </Col>
+            <Col xs={12} md={6} className="mt-4 mt-md-0">
+              <Widget className="widget-p-md"><div> Widget </div></Widget>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   )
 }
