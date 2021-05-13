6cf194aad573ec9db816ade82f3b51b9f04c152d
yurykurylovich <yury.kurylovich@gmail.com>
2021-05-13 17:54:58 +0300

    Added badge icon component, finished Badges page

--- src/pages/uielements/badges/Badges.js
+++ src/pages/uielements/badges/Badges.js
@@ -5,7 +5,7 @@ import {
   Badge
 } from "reactstrap";
 import Widget from "../../../components/Widget/Widget.js";
-import starIcon from "../../../assets/badgesIcons/starIcon.svg";
+import StarIcon from "../../../assets/badges/StarIcon.js";
 import s from "./Badges.module.scss";
 
 export default function Badges() {
@@ -36,15 +36,15 @@ export default function Badges() {
                 <div className="headline-2">Contextual Badges with icons</div>
                 <div className="mt-2 mb-3">Add any icon to make the badge more enjoyable and informative. If no color is specified <code>default</code> will be used.</div>
                 <div className={s.badgesBlock}>
-                  <Badge color="primary"><img src={starIcon} alt="star"/>Primary</Badge>
-                  <Badge><img src={starIcon} alt="star"/>Default</Badge>
-                  <Badge color="secondary-red"><img src={starIcon} alt="star"/>Sec. Red</Badge>
-                  <Badge color="secondary-yellow"><img src={starIcon} alt="star"/>Sec. Yellow</Badge>
-                  <Badge color="secondary-cyan"><img src={starIcon} alt="star"/>Sec. Cyan</Badge>
-                  <Badge color="success"><img src={starIcon} alt="star"/>Success</Badge>
-                  <Badge color="info"><img src={starIcon} alt="star"/>Info</Badge>
-                  <Badge color="warning"><img src={starIcon} alt="star"/>Warning</Badge>
-                  <Badge color="danger"><img src={starIcon} alt="star"/>Danger</Badge>
+                  <Badge color="primary"><StarIcon/>Primary</Badge>
+                  <Badge><StarIcon/>Default</Badge>
+                  <Badge color="secondary-red"><StarIcon/>Sec. Red</Badge>
+                  <Badge color="secondary-yellow"><StarIcon/>Sec. Yellow</Badge>
+                  <Badge color="secondary-cyan"><StarIcon/>Sec. Cyan</Badge>
+                  <Badge color="success"><StarIcon/>Success</Badge>
+                  <Badge color="info"><StarIcon/>Info</Badge>
+                  <Badge color="warning"><StarIcon/>Warning</Badge>
+                  <Badge color="danger"><StarIcon/>Danger</Badge>
                 </div>
               </Widget>
             </Col>
@@ -52,8 +52,8 @@ export default function Badges() {
           <Row className="gutter mb-4">
             <Col xs={12} md={6}>
               <Widget className="widget-p-md">
-                <div className="headline-2">Contextual Badges</div>
-                <div className="mt-2 mb-3">Add modifier <code>color="*"</code> property to change the appearance of a badge. If no color is specified <code>default</code> will be used.</div>
+                <div className="headline-2">Inverted Badges</div>
+                <div className="mt-2 mb-3">Add <code>badge-inverse-"*"</code> class to change the style of a badge. If no color is specified <code>default</code> will be used.</div>
                 <div className={s.badgesBlock}>
                   <Badge className="badge-inverse-primary">Primary</Badge>
                   <Badge className="badge-inverse-default">Default</Badge>
@@ -69,26 +69,26 @@ export default function Badges() {
             </Col>
             <Col xs={12} md={6} className="mt-4 mt-md-0">
               <Widget className="widget-p-md">
-                <div className="headline-2">Contextual Badges with icons</div>
-                <div className="mt-2 mb-3">Add any icon to make the badge more enjoyable and informative. If no color is specified <code>default</code> will be used.</div>
+                <div className="headline-2">Inverted Badges with icons</div>
+                <div className="mt-2 mb-3">Add <code>badge-inverse-"*"</code> class to change the style of a badge. Add <code>pill</code> property to make badges rounded. Add icon to make it more informative and enjoyable.</div>
                 <div className={s.badgesBlock}>
-                  <Badge className="badge-inverse-primary" color="primary"><img src={starIcon} alt="star"/>Primary</Badge>
-                  <Badge><img src={starIcon} alt="star"/>Default</Badge>
-                  <Badge color="secondary-red"><img src={starIcon} alt="star"/>Sec. Red</Badge>
-                  <Badge color="secondary-yellow"><img src={starIcon} alt="star"/>Sec. Yellow</Badge>
-                  <Badge color="secondary-cyan"><img src={starIcon} alt="star"/>Sec. Cyan</Badge>
-                  <Badge color="success"><img src={starIcon} alt="star"/>Success</Badge>
-                  <Badge color="info"><img src={starIcon} alt="star"/>Info</Badge>
-                  <Badge color="warning"><img src={starIcon} alt="star"/>Warning</Badge>
-                  <Badge color="danger"><img src={starIcon} alt="star"/>Danger</Badge>
+                  <Badge className="badge-inverse-primary" pill color="primary"><StarIcon/>Primary</Badge>
+                  <Badge className="badge-inverse-default" pill><StarIcon/>Default</Badge>
+                  <Badge className="badge-inverse-secondary-red" pill><StarIcon/>Sec. Red</Badge>
+                  <Badge className="badge-inverse-secondary-yellow" pill><StarIcon/>Sec. Yellow</Badge>
+                  <Badge className="badge-inverse-secondary-cyan" pill><StarIcon/>Sec. Cyan</Badge>
+                  <Badge className="badge-inverse-success" pill><StarIcon/>Success</Badge>
+                  <Badge className="badge-inverse-info" pill><StarIcon/>Info</Badge>
+                  <Badge className="badge-inverse-warning" pill><StarIcon/>Warning</Badge>
+                  <Badge className="badge-inverse-danger" pill><StarIcon/>Danger</Badge>
                 </div>
               </Widget>            </Col>
           </Row>
           <Row className="gutter">
             <Col xs={12} md={6}>
               <Widget className="widget-p-md">
-                <div className="headline-2">Contextual Badges</div>
-                <div className="mt-2 mb-3">Add modifier <code>color="*"</code> property to change the appearance of a badge. If no color is specified <code>default</code> will be used.</div>
+                <div className="headline-2">Rounded Contextual Badges</div>
+                <div className="mt-2 mb-3">Add modifier <code>color="*"</code> property to change the appearance of a badge. Add <code>pill</code> property to make badges rounded. Add icon to make it more informative and enjoyable. If no color is specified <code>default</code> will be used.</div>
                 <div className={s.badgesBlock}>
                   <Badge href="#" pill color="primary">Primary</Badge>
                   <Badge href="#" pill>Default</Badge>
@@ -104,18 +104,18 @@ export default function Badges() {
             </Col>
             <Col xs={12} md={6} className="mt-4 mt-md-0">
               <Widget className="widget-p-md">
-                <div className="headline-2">Contextual Badges with icons</div>
-                <div className="mt-2 mb-3">Add any icon to make the badge more enjoyable and informative. If no color is specified <code>default</code> will be used.</div>
+                <div className="headline-2">Rounded Contextual Badges with icons</div>
+                <div className="mt-2 mb-3">Add any icon to make the badge more enjoyable and informative. Add <code>pill</code> property to make badges rounded. Add icon to make it more informative and enjoyable.</div>
                 <div className={s.badgesBlock}>
-                  <Badge href="#" pill color="primary"><img src={starIcon} alt="star"/>Primary</Badge>
-                  <Badge href="#" pill><img src={starIcon} alt="star"/>Default</Badge>
-                  <Badge href="#" pill color="secondary-red"><img src={starIcon} alt="star"/>Sec. Red</Badge>
-                  <Badge href="#" pill color="secondary-yellow"><img src={starIcon} alt="star"/>Sec. Yellow</Badge>
-                  <Badge href="#" pill color="secondary-cyan"><img src={starIcon} alt="star"/>Sec. Cyan</Badge>
-                  <Badge href="#" pill color="success"><img src={starIcon} alt="star"/>Success</Badge>
-                  <Badge href="#" pill color="info"><img src={starIcon} alt="star"/>Info</Badge>
-                  <Badge href="#" pill color="warning"><img src={starIcon} alt="star"/>Warning</Badge>
-                  <Badge href="#" pill color="danger"><img src={starIcon} alt="star"/>Danger</Badge>
+                  <Badge href="#" pill color="primary"><StarIcon/>Primary</Badge>
+                  <Badge href="#" pill><StarIcon/>Default</Badge>
+                  <Badge href="#" pill color="secondary-red"><StarIcon/>Sec. Red</Badge>
+                  <Badge href="#" pill color="secondary-yellow"><StarIcon/>Sec. Yellow</Badge>
+                  <Badge href="#" pill color="secondary-cyan"><StarIcon/>Sec. Cyan</Badge>
+                  <Badge href="#" pill color="success"><StarIcon/>Success</Badge>
+                  <Badge href="#" pill color="info"><StarIcon/>Info</Badge>
+                  <Badge href="#" pill color="warning"><StarIcon/>Warning</Badge>
+                  <Badge href="#" pill color="danger"><StarIcon/>Danger</Badge>
                 </div>
               </Widget>
             </Col>
