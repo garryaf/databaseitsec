33b95188b0048dceb0a91c953c478247b43c9005
yurykurylovich <yury.kurylovich@gmail.com>
2021-05-14 15:12:55 +0300

    Added mixin for buttons with icons, finished buttons page

--- src/pages/uielements/buttons/Buttons.js
+++ src/pages/uielements/buttons/Buttons.js
@@ -4,8 +4,11 @@ import {
   Row,
   Button,
 } from "reactstrap";
-import s from "./Buttons.module.scss";
 import Widget from "../../../components/Widget/Widget.js";
+import OutlineStar from "../../../assets/buttons/OutlineStarIcon.js";
+
+import s from "./Buttons.module.scss";
+
 
 export default function Buttons() {
   return (
@@ -15,35 +18,42 @@ export default function Buttons() {
           <Row className="gutter mb-4">
             <Col xs={12} md={6}>
               <Widget className="widget-p-md">
-                <div className="headline-2">Default Buttons</div>
-                <div className="mt-2 mb-3">Bootstrap includes predefined button styles, each serving its own semantic purpose.</div>
+                <div className="headline-2">Rounded Buttons</div>
+                <div className="mt-2 mb-3">
+                  Bootstrap includes predefined button styles, each serving its own semantic purpose.
+                  Use <code>btn-rounded</code> class to make button rounded.
+                </div>
                 <div className={s.buttonsBlock}>
-                  <Button color="primary" className="btn-rounded-f">Primary</Button>
-                  <Button className="btn-rounded-f">Default</Button>
-                  <Button color="secondary-red" className="btn-rounded-f">Sec. Red</Button>
-                  <Button color="secondary-yellow" className="btn-rounded-f">Sec. Yellow</Button>
-                  <Button color="secondary-cyan" className="btn-rounded-f">Sec. Cyan</Button>
-                  <Button color="success" className="btn-rounded-f">Success</Button>
-                  <Button color="info" className="btn-rounded-f">Info</Button>
-                  <Button color="warning" className="btn-rounded-f">Warning</Button>
-                  <Button color="danger" className="btn-rounded-f">Danger</Button>
+                  <Button color="primary" className="btn-rounded">Primary</Button>
+                  <Button className="btn-rounded">Default</Button>
+                  <Button color="secondary-red" className="btn-rounded">Sec. Red</Button>
+                  <Button color="secondary-yellow" className="btn-rounded">Sec. Yellow</Button>
+                  <Button color="secondary-cyan" className="btn-rounded">Sec. Cyan</Button>
+                  <Button color="success" className="btn-rounded">Success</Button>
+                  <Button color="info" className="btn-rounded">Info</Button>
+                  <Button color="warning" className="btn-rounded">Warning</Button>
+                  <Button color="danger" className="btn-rounded">Danger</Button>
                 </div>
               </Widget>
             </Col>
             <Col xs={12} md={6} className="mt-4 mt-md-0">
               <Widget className="widget-p-md">
-                <div className="headline-2">Outlined Buttons</div>
-                <div className="mt-2 mb-3">Add any icon to make the badge more enjoyable and informative. If no color is specified <code>default</code> will be used.</div>
+                <div className="headline-2">Outlined Rounded Buttons</div>
+                <div className="mt-2 mb-3">
+                  In need of a button, but not the hefty background colors they bring?
+                  Use <code>outline</code> property to remove all
+                  background colors on any button.
+                </div>
                 <div className={s.buttonsBlock}>
-                  <Button outline color="primary" className="btn-rounded-f">Primary</Button>
-                  <Button outline className="btn-rounded-f">Default</Button>
-                  <Button outline color="secondary-red" className="btn-rounded-f">Sec. Red</Button>
-                  <Button outline color="secondary-yellow" className="btn-rounded-f">Sec. Yellow</Button>
-                  <Button outline color="secondary-cyan" className="btn-rounded-f">Sec. Cyan</Button>
-                  <Button outline color="success" className="btn-rounded-f">Success</Button>
-                  <Button outline color="info" className="btn-rounded-f">Info</Button>
-                  <Button outline color="warning" className="btn-rounded-f">Warning</Button>
-                  <Button outline color="danger" className="btn-rounded-f">Danger</Button>
+                  <Button outline color="primary" className="btn-rounded">Primary</Button>
+                  <Button outline className="btn-rounded">Default</Button>
+                  <Button outline color="secondary-red" className="btn-rounded">Sec. Red</Button>
+                  <Button outline color="secondary-yellow" className="btn-rounded">Sec. Yellow</Button>
+                  <Button outline color="secondary-cyan" className="btn-rounded">Sec. Cyan</Button>
+                  <Button outline color="success" className="btn-rounded">Success</Button>
+                  <Button outline color="info" className="btn-rounded">Info</Button>
+                  <Button outline color="warning" className="btn-rounded">Warning</Button>
+                  <Button outline color="danger" className="btn-rounded">Danger</Button>
                 </div>
               </Widget>
             </Col>
@@ -51,19 +61,40 @@ export default function Buttons() {
           <Row className="gutter mb-4">
             <Col xs={12} md={6}>
               <Widget className="widget-p-md">
-                <div className="headline-2">Inverted Badges</div>
-                <div className="mt-2 mb-3">Add <code>badge-inverse-"*"</code> class to change the style of a badge. If no color is specified <code>default</code> will be used.</div>
-                <div >
-
+                <div className="headline-2">Default Buttons</div>
+                <div className="mt-2 mb-3">
+                  Use any of the available button classes to quickly create a styled button. Semantically distinguishable beauty.
+                </div>
+                <div className={s.buttonsBlock}>
+                  <Button color="primary" >Primary</Button>
+                  <Button>Default</Button>
+                  <Button color="secondary-red">Sec. Red</Button>
+                  <Button color="secondary-yellow">Sec. Yellow</Button>
+                  <Button color="secondary-cyan">Sec. Cyan</Button>
+                  <Button color="success">Success</Button>
+                  <Button color="info">Info</Button>
+                  <Button color="warning">Warning</Button>
+                  <Button color="danger">Danger</Button>
                 </div>
               </Widget>
             </Col>
             <Col xs={12} md={6} className="mt-4 mt-md-0">
               <Widget className="widget-p-md">
-                <div className="headline-2">Inverted Badges with icons</div>
-                <div className="mt-2 mb-3">Add <code>badge-inverse-"*"</code> class to change the style of a badge. Add <code>pill</code> property to make badges rounded. Add icon to make it more informative and enjoyable.</div>
-                <div >
-
+                <div className="headline-2">Outlined Default Buttons</div>
+                <div className="mt-2 mb-3">
+                  This is the default button style, but you can make them more rounded.
+                  Use whichever button shape you like best.
+                </div>
+                <div className={s.buttonsBlock}>
+                  <Button outline color="primary" >Primary</Button>
+                  <Button outline>Default</Button>
+                  <Button outline color="secondary-red">Sec. Red</Button>
+                  <Button outline color="secondary-yellow">Sec. Yellow</Button>
+                  <Button outline color="secondary-cyan">Sec. Cyan</Button>
+                  <Button outline color="success">Success</Button>
+                  <Button outline color="info">Info</Button>
+                  <Button outline color="warning">Warning</Button>
+                  <Button outline color="danger">Danger</Button>
                 </div>
               </Widget>
             </Col>
@@ -71,19 +102,41 @@ export default function Buttons() {
           <Row className="gutter">
             <Col xs={12} md={6}>
               <Widget className="widget-p-md">
-                <div className="headline-2">Rounded Contextual Badges</div>
-                <div className="mt-2 mb-3">Add modifier <code>color="*"</code> property to change the appearance of a badge. Add <code>pill</code> property to make badges rounded. Add icon to make it more informative and enjoyable. If no color is specified <code>default</code> will be used.</div>
-                <div >
-
+                <div className="headline-2">Buttons With Icons</div>
+                <div className="mt-2 mb-3">
+                  Do more with icons! Icons or Icon components may be used in buttons.
+                  Let your buttons be more informative and enjoyable!
+                </div>
+                <div className={s.buttonsBlock}>
+                  <Button color="primary" className="btn-rounded with-icon"><OutlineStar/>Primary</Button>
+                  <Button className="btn-rounded with-icon"><OutlineStar/>Default</Button>
+                  <Button color="secondary-red" className="btn-rounded with-icon"><OutlineStar/>Sec. Red</Button>
+                  <Button color="secondary-yellow" className="btn-rounded with-icon"><OutlineStar/>Sec. Yellow</Button>
+                  <Button color="secondary-cyan" className="btn-rounded with-icon"><OutlineStar/>Sec. Cyan</Button>
+                  <Button color="success" className="btn-rounded with-icon"><OutlineStar/>Success</Button>
+                  <Button color="info" className="btn-rounded with-icon"><OutlineStar/>Info</Button>
+                  <Button color="warning" className="btn-rounded with-icon"><OutlineStar/>Warning</Button>
+                  <Button color="danger" className="btn-rounded with-icon"><OutlineStar/>Danger</Button>
                 </div>
               </Widget>
             </Col>
             <Col xs={12} md={6} className="mt-4 mt-md-0">
               <Widget className="widget-p-md">
-                <div className="headline-2">Rounded Contextual Badges with icons</div>
-                <div className="mt-2 mb-3">Add any icon to make the badge more enjoyable and informative. Add <code>pill</code> property to make badges rounded. Add icon to make it more informative and enjoyable.</div>
-                <div >
-
+                <div className="headline-2">Outlined Buttons With Icons</div>
+                <div className="mt-2 mb-3">
+                  In need of a button, but not the hefty background colors they bring?
+                  Use <code>outline</code> property, add icon. Style the buttons however you like!
+                </div>
+                <div className={s.buttonsBlock}>
+                  <Button outline color="primary" className="btn-rounded with-icon"><OutlineStar/>Primary</Button>
+                  <Button outline color="secondary" className="btn-rounded with-icon"><OutlineStar/>Default</Button>
+                  <Button outline color="secondary-red" className="btn-rounded with-icon"><OutlineStar/>Sec. Red</Button>
+                  <Button outline color="secondary-yellow" className="btn-rounded with-icon"><OutlineStar/>Sec. Yellow</Button>
+                  <Button outline color="secondary-cyan" className="btn-rounded with-icon"><OutlineStar/>Sec. Cyan</Button>
+                  <Button outline color="success" className="btn-rounded with-icon"><OutlineStar/>Success</Button>
+                  <Button outline color="info" className="btn-rounded with-icon"><OutlineStar/>Info</Button>
+                  <Button outline color="warning" className="btn-rounded with-icon"><OutlineStar/>Warning</Button>
+                  <Button outline color="danger" className="btn-rounded with-icon"><OutlineStar/>Danger</Button>
                 </div>
               </Widget>
             </Col>
