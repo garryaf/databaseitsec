e3e6108f1cbe0efba0e000f25ca9eace05b96ea8
yurykurylovich <yury.kurylovich@gmail.com>
2021-06-14 18:44:31 +0300

    Added recharts other charts

--- src/pages/charts/other/OtherCharts.js
+++ src/pages/charts/other/OtherCharts.js
@@ -9,10 +9,33 @@ import {
   NavItem,
   NavLink,
 } from "reactstrap";
+import {
+  ComposedChart,
+  ScatterChart,
+  Scatter,
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Treemap,
+  Bar,
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from "recharts";
+
 import ApexCharts from "react-apexcharts";
 import Widget from "../../../components/Widget/Widget";
+import s from "./OtherCharts.module.scss";
 import chartsData from "./mock";
-
 
 export default function OtherCharts() {
 
@@ -26,7 +49,7 @@ export default function OtherCharts() {
   }
 
   return (
-    <Widget className="charts-tabs-widget">
+    <Widget className="charts-tabs-widget" style={{overflow: "auto"}}>
       <Nav tabs className="mb-5">
         <NavItem>
           <NavLink
@@ -103,14 +126,104 @@ export default function OtherCharts() {
           </Col>
         </TabPane>
         <TabPane tabId={2}>
-          <Row>
-            <Col xs={12} lg={6}>
-              <Widget><div>Hi</div></Widget>
-            </Col>
-            <Col xs={12} lg={6}>
-              <Widget><div>Hi</div></Widget>
-            </Col>
-          </Row>
+          <Col>
+            <Row className="mb-5">
+              <Col xs={12} lg={6}>
+                <Widget>
+                  <div className="headline-2 mb-4">Line Bar Area Chart</div>
+                  <div className={s.rechartsBlock}>
+                    <ResponsiveContainer width="100%" height="100%">
+                      <ComposedChart
+                        width={500}
+                        height={400}
+                        data={recharts.composedChart.data}
+                        margin={{
+                          top: 20,
+                          right: 20,
+                          bottom: 20,
+                          left: 20,
+                        }}
+                      >
+                        <CartesianGrid stroke="#f5f5f5" />
+                        <XAxis dataKey="name" scale="band" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Area type="monotone" dataKey="amt" fill="#C7D0D9" stroke="#6b859e" />
+                        <Bar dataKey="pv" barSize={30} fill="#4d53e0" />
+                        <Line type="monotone" dataKey="uv" stroke="#FFA100" />
+                        <Scatter dataKey="cnt" fill="#FF5668" />
+                      </ComposedChart>
+                    </ResponsiveContainer>
+                  </div>
+                </Widget>
+              </Col>
+              <Col xs={12} lg={6}>
+                <Widget>
+                  <div className="headline-2 mb-4">Radar Chart</div>
+                  <div className={s.rechartsBlock}>
+                    <ResponsiveContainer width="100%" height="100%">
+                      <RadarChart cx="50%" cy="50%" outerRadius="80%" data={recharts.radarChart.data}>
+                        <PolarGrid />
+                        <PolarAngleAxis dataKey="subject" />
+                        <PolarRadiusAxis angle={30} domain={[0, 150]} />
+                        <Radar name="John" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.5} />
+                        <Radar name="Lily" dataKey="B" stroke="#FF5668" fill="#FF5668" fillOpacity={0.5} />
+                        <Legend />
+                      </RadarChart>
+                    </ResponsiveContainer>
+                  </div>
+                </Widget>
+              </Col>
+            </Row>
+            <Row className="mb-4">
+              <Col xs={12} lg={6}>
+                <Widget>
+                  <div className="headline-2 mb-4">Scatter Chart</div>
+                  <div className={s.rechartsBlock}>
+                    <ResponsiveContainer width="100%" height="100%">
+                      <ScatterChart
+                        width={500}
+                        height={400}
+                        margin={{
+                          top: 20,
+                          right: 20,
+                          bottom: 20,
+                          left: 20,
+                        }}
+                      >
+                        <CartesianGrid />
+                        <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+                        <YAxis yAxisId="left" type="number" dataKey="y" name="weight" unit="kg" stroke="#8884d8" />
+                        <YAxis
+                          yAxisId="right"
+                          type="number"
+                          dataKey="y"
+                          name="weight"
+                          unit="kg"
+                          orientation="right"
+                          stroke="#FFC405"
+                        />
+                        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                        <Scatter yAxisId="left" name="A school" data={recharts.scatterChart.data1} fill="#4d53e0" />
+                        <Scatter yAxisId="right" name="A school" data={recharts.scatterChart.data2} fill="#FFA100" />
+                      </ScatterChart>
+                    </ResponsiveContainer>
+                  </div>
+                </Widget>
+              </Col>
+              <Col xs={12} lg={6}>
+                <Widget>
+                  <div className="headline-2 mb-4">Treemap Chart</div>
+                  <div className={s.rechartsBlock}>
+                    <ResponsiveContainer width="100%" height="100%">
+                      <Treemap width={400} height={200} data={recharts.treemapChart.data} dataKey="size" ratio={4 / 3} stroke="#fff" fill="#4d53e0" />
+                    </ResponsiveContainer>
+                  </div>
+                </Widget>
+              </Col>
+            </Row>
+          </Col>
         </TabPane>
         <TabPane tabId={3}>
           <Row>
--- src/pages/charts/other/mock.js
+++ src/pages/charts/other/mock.js
@@ -192,7 +192,250 @@ export default {
     },
   },
   recharts: {
-
+    composedChart: {
+      data: [
+        {
+          name: 'Page A',
+          uv: 590,
+          pv: 800,
+          amt: 1400,
+          cnt: 490,
+        },
+        {
+          name: 'Page B',
+          uv: 868,
+          pv: 967,
+          amt: 1506,
+          cnt: 590,
+        },
+        {
+          name: 'Page C',
+          uv: 1397,
+          pv: 1098,
+          amt: 989,
+          cnt: 350,
+        },
+        {
+          name: 'Page D',
+          uv: 1480,
+          pv: 1200,
+          amt: 1228,
+          cnt: 480,
+        },
+        {
+          name: 'Page E',
+          uv: 1520,
+          pv: 1108,
+          amt: 1100,
+          cnt: 460,
+        },
+        {
+          name: 'Page F',
+          uv: 1400,
+          pv: 680,
+          amt: 1700,
+          cnt: 380,
+        },
+      ],
+    },
+    treemapChart: {
+      data: [
+        {
+          name: 'axis',
+          children: [
+            { name: 'Axes', size: 1302 },
+            { name: 'Axis', size: 24593 },
+            { name: 'AxisGridLine', size: 652 },
+            { name: 'AxisLabel', size: 636 },
+            { name: 'CartesianAxes', size: 6703 },
+          ],
+        },
+        {
+          name: 'controls',
+          children: [
+            { name: 'AnchorControl', size: 2138 },
+            { name: 'ClickControl', size: 3824 },
+            { name: 'Control', size: 1353 },
+            { name: 'ControlList', size: 4665 },
+            { name: 'DragControl', size: 2649 },
+            { name: 'ExpandControl', size: 2832 },
+            { name: 'HoverControl', size: 4896 },
+            { name: 'IControl', size: 763 },
+            { name: 'PanZoomControl', size: 5222 },
+            { name: 'SelectionControl', size: 7862 },
+            { name: 'TooltipControl', size: 8435 },
+          ],
+        },
+        {
+          name: 'data',
+          children: [
+            { name: 'Data', size: 20544 },
+            { name: 'DataList', size: 19788 },
+            { name: 'DataSprite', size: 10349 },
+            { name: 'EdgeSprite', size: 3301 },
+            { name: 'NodeSprite', size: 19382 },
+            {
+              name: 'render',
+              children: [
+                { name: 'ArrowType', size: 698 },
+                { name: 'EdgeRenderer', size: 5569 },
+                { name: 'IRenderer', size: 353 },
+                { name: 'ShapeRenderer', size: 2247 },
+              ],
+            },
+            { name: 'ScaleBinding', size: 11275 },
+            { name: 'Tree', size: 7147 },
+            { name: 'TreeBuilder', size: 9930 },
+          ],
+        },
+        {
+          name: 'events',
+          children: [
+            { name: 'DataEvent', size: 7313 },
+            { name: 'SelectionEvent', size: 6880 },
+            { name: 'TooltipEvent', size: 3701 },
+            { name: 'VisualizationEvent', size: 2117 },
+          ],
+        },
+        {
+          name: 'legend',
+          children: [
+            { name: 'Legend', size: 20859 },
+            { name: 'LegendItem', size: 4614 },
+            { name: 'LegendRange', size: 10530 },
+          ],
+        },
+        {
+          name: 'operator',
+          children: [
+            {
+              name: 'distortion',
+              children: [
+                { name: 'BifocalDistortion', size: 4461 },
+                { name: 'Distortion', size: 6314 },
+                { name: 'FisheyeDistortion', size: 3444 },
+              ],
+            },
+            {
+              name: 'encoder',
+              children: [
+                { name: 'ColorEncoder', size: 3179 },
+                { name: 'Encoder', size: 4060 },
+                { name: 'PropertyEncoder', size: 4138 },
+                { name: 'ShapeEncoder', size: 1690 },
+                { name: 'SizeEncoder', size: 1830 },
+              ],
+            },
+            {
+              name: 'filter',
+              children: [
+                { name: 'FisheyeTreeFilter', size: 5219 },
+                { name: 'GraphDistanceFilter', size: 3165 },
+                { name: 'VisibilityFilter', size: 3509 },
+              ],
+            },
+            { name: 'IOperator', size: 1286 },
+            {
+              name: 'label',
+              children: [
+                { name: 'Labeler', size: 9956 },
+                { name: 'RadialLabeler', size: 3899 },
+                { name: 'StackedAreaLabeler', size: 3202 },
+              ],
+            },
+            {
+              name: 'layout',
+              children: [
+                { name: 'AxisLayout', size: 6725 },
+                { name: 'BundledEdgeRouter', size: 3727 },
+                { name: 'CircleLayout', size: 9317 },
+                { name: 'CirclePackingLayout', size: 12003 },
+                { name: 'DendrogramLayout', size: 4853 },
+                { name: 'ForceDirectedLayout', size: 8411 },
+                { name: 'IcicleTreeLayout', size: 4864 },
+                { name: 'IndentedTreeLayout', size: 3174 },
+                { name: 'Layout', size: 7881 },
+                { name: 'NodeLinkTreeLayout', size: 12870 },
+                { name: 'PieLayout', size: 2728 },
+                { name: 'RadialTreeLayout', size: 12348 },
+                { name: 'RandomLayout', size: 870 },
+                { name: 'StackedAreaLayout', size: 9121 },
+                { name: 'TreeMapLayout', size: 9191 },
+              ],
+            },
+            { name: 'Operator', size: 2490 },
+            { name: 'OperatorList', size: 5248 },
+            { name: 'OperatorSequence', size: 4190 },
+            { name: 'OperatorSwitch', size: 2581 },
+            { name: 'SortOperator', size: 2023 },
+          ],
+        },
+      ],
+    },
+    radarChart: {
+      data: [
+        {
+          subject: 'Math',
+          A: 140,
+          B: 100,
+          fullMark: 150,
+        },
+        {
+          subject: 'Chinese',
+          A: 130,
+          B: 80,
+          fullMark: 150,
+        },
+        {
+          subject: 'English',
+          A: 96,
+          B: 120,
+          fullMark: 150,
+        },
+        {
+          subject: 'Geography',
+          A: 70,
+          B: 100,
+          fullMark: 150,
+        },
+        {
+          subject: 'Physics',
+          A: 85,
+          B: 120,
+          fullMark: 150,
+        },
+        {
+          subject: 'History',
+          A: 65,
+          B: 115,
+          fullMark: 150,
+        },
+      ],
+    },
+    scatterChart: {
+      data1: [
+        { x: 100, y: 200, z: 200 },
+        { x: 120, y: 100, z: 260 },
+        { x: 170, y: 300, z: 400 },
+        { x: 140, y: 250, z: 280 },
+        { x: 150, y: 400, z: 500 },
+        { x: 110, y: 280, z: 200 },
+      ],
+      data2: [
+        { x: 300, y: 300, z: 200 },
+        { x: 400, y: 500, z: 260 },
+        { x: 200, y: 700, z: 400 },
+        { x: 340, y: 350, z: 280 },
+        { x: 560, y: 500, z: 500 },
+        { x: 230, y: 780, z: 200 },
+        { x: 500, y: 400, z: 200 },
+        { x: 300, y: 500, z: 260 },
+        { x: 240, y: 300, z: 400 },
+        { x: 320, y: 550, z: 280 },
+        { x: 500, y: 400, z: 500 },
+        { x: 420, y: 280, z: 200 },
+      ],
+    },
   },
   echarts: {
 
