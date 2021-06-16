3fbf38f3a90fd565497ea1313eb7d3627a699a3a
yurykurylovich <yury.kurylovich@gmail.com>
2021-06-16 17:43:17 +0300

    Added basic styles to Add Event widget

--- src/pages/extra/calendar/Calendar.js
+++ src/pages/extra/calendar/Calendar.js
@@ -3,9 +3,14 @@ import {
   Col,
   Row,
   Button,
+  Label,
+  Input,
   Card,
   CardBody,
   ButtonGroup,
+  ListGroup,
+  FormGroup,
+  ListGroupItem,
 } from "reactstrap";
 import Widget from "../../../components/Widget/Widget";
 import s from "./Calendar.module.scss";
@@ -13,6 +18,101 @@ import s from "./Calendar.module.scss";
 export default function Calendar() {
 
   return (
-    <div>Hello</div>
+    <div>
+      <Row className="gutter mb-4">
+        <Col xs={12} lg={9}>
+          <Widget className="widget-p-none">
+            Hello
+          </Widget>
+        </Col>
+        <Col xs={12} lg={3} className="d-none d-lg-block">
+          <Widget className="widget-p-md">
+            <div className="headline-2 mb-1">Calendars</div>
+            <div className="body-3 muted mb-4">Select the calendar whose events you want to see</div>
+            <ListGroup className="mb-4 ml-1">
+              <div className="checkbox abc-checkbox-primary pl-3">
+                <Input
+                  id="checkbox0"
+                  type="checkbox"
+                  className="styled"
+                  checked
+                />
+                <Label htmlFor="checkbox0" className="body-3">View All</Label>
+              </div>
+              <div className="checkbox abc-checkbox-warning pl-3">
+                <Input
+                  id="checkbox1"
+                  type="checkbox"
+                  className="styled"
+                  checked
+                />
+                <Label htmlFor="checkbox1" className="body-3">Flatlogic</Label>
+              </div>
+              <div className="checkbox abc-checkbox-info pl-3">
+                <Input
+                  id="checkbox2"
+                  type="checkbox"
+                  className="styled"
+                  checked
+                />
+                <Label htmlFor="checkbox2" className="body-3">Personal</Label>
+              </div>
+              <div className="checkbox abc-checkbox-danger pl-3">
+                <Input
+                  id="checkbox3"
+                  type="checkbox"
+                  className="styled"
+                  checked
+                />
+                <Label htmlFor="checkbox3" className="body-3">Holiday</Label>
+              </div>
+              <div className="checkbox abc-checkbox-success pl-3">
+                <Input
+                  id="checkbox4"
+                  type="checkbox"
+                  className="styled"
+                  checked
+                />
+                <Label htmlFor="checkbox4" className="body-3">Business</Label>
+              </div>
+            </ListGroup>
+            <div className="mt-3 d-flex justify-content-center">
+              <Button className="btn-rounded btn-secondary-red body-2">
+                Add Event
+              </Button>
+            </div>
+          </Widget>
+        </Col>
+      </Row>
+    </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <div className="checkbox checkbox-primary">
+//   <input
+//     id="checkbox0"
+//     type="checkbox"
+//     className="styled"
+//     checked={this.state.checkboxes[0]}
+//     onChange={(event) =>
+//       this.changeCheck(event, "checkboxes", 0)
+//     }
+//   />
+//   <label htmlFor="checkbox0" />
+// </div>
