f486b5af77d495e672f16e9f6ad720c9cb467ffa
yurykurylovich <yury.kurylovich@gmail.com>
2021-06-16 15:53:26 +0300

    Init Calendar page

--- src/pages/extra/calendar/Calendar.js
+++ src/pages/extra/calendar/Calendar.js
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Col,
+  Row,
+  Button,
+  Card,
+  CardBody,
+  ButtonGroup,
+} from "reactstrap";
+import Widget from "../../../components/Widget/Widget";
+import s from "./Calendar.module.scss";
 
 export default function Calendar() {
 
