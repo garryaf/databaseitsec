41bc684bdf7a1244308090aafb16cb47fe003255
yurykurylovich <yury.kurylovich@gmail.com>
2021-10-08 01:03:25 +0300

    Update UsersListTable component

--- src/components/Users/list/UsersListTable.js
+++ src/components/Users/list/UsersListTable.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import * as dataFormat from "./UsersDataFormatters";
@@ -17,43 +17,180 @@ import {
 } from 'reactstrap';
 
 import {
-  BootstrapTable,
+  // BootstrapTable,
   TableHeaderColumn,
   SearchField
 } from "react-bootstrap-table";
+import BootstrapTable from "react-bootstrap-table-next";
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 import Widget from "../../Widget/Widget";
 
 import s from "../Users.module.scss";
 
-class UsersListTable extends Component {
-  state = {
-    modalOpen: false,
-    idToDelete: null,
-  }
+const { SearchBar } = Search;
 
-  handleDelete() {
-    const userId = this.props.idToDelete;
-    this.props.dispatch(actions.doDelete(userId));
-  }
+// class UsersListTable extends Component {
+//   state = {
+//     modalOpen: false,
+//     idToDelete: null,
+//   }
+//
+//   handleDelete() {
+//     const userId = this.props.idToDelete;
+//     this.props.dispatch(actions.doDelete(userId));
+//   }
+//   openModal(cell) {
+//     const userId = cell;
+//     this.props.dispatch(actions.doOpenConfirm(userId));
+//   }
+//   closeModal() {
+//     this.props.dispatch(actions.doCloseConfirm());
+//   }
+//   actionFormatter(cell) {
+//     return (
+//       <div className={`d-flex justify-content-between`}>
+//         <Button
+//           className={s.controlBtn}
+//           color="info"
+//           size="xs"
+//           onClick={() => this.props.dispatch(push(`/admin/users/${cell}`))}
+//         >
+//           View
+//         </Button>
+//         <Button
+//           className={`${s.controlBtn} mx-2`}
+//           color="success"
+//           size="xs"
+//           onClick={() => this.props.dispatch(push(`/admin/users/${cell}/edit`))}
+//         >
+//           Edit
+//         </Button>
+//         <Button
+//           className={s.controlBtn}
+//           color="danger"
+//           size="xs"
+//           onClick={() => this.openModal(cell)}
+//         >
+//           Delete
+//         </Button>
+//       </div>
+//     )
+//   }
+//   componentDidMount() {
+//     const { dispatch } = this.props;
+//     dispatch(actions.doFetch({}));
+//   }
+//   renderSizePageDropdown = (props) => {
+//     const limits = [];
+//     props.sizePerPageList.forEach((limit) => {
+//       limits.push(<DropdownItem key={limit} onClick={() => props.changeSizePerPage(limit)}>{ limit }</DropdownItem>)
+//     });
+//
+//     return (
+//       <ButtonDropdown isOpen={props.open} toggle={props.toggleDropDown}>
+//         <DropdownToggle caret>
+//           { props.currSizePerPage }
+//         </DropdownToggle>
+//         <DropdownMenu>
+//           { limits }
+//         </DropdownMenu>
+//       </ButtonDropdown>
+//     );
+//   };
+//   createCustomSearchField = (props) => {
+//     return (
+//       <SearchField
+//         className="mb-sm-5 mr-sm-0"
+//         placeholder="Search"
+//       />
+//     );
+//   }
+//   render() {
+//     const { rows } = this.props;
+//
+//     const options = {
+//       sizePerPage: 10,
+//       paginationSize: 5,
+//       searchField: this.createCustomSearchField,
+//       sizePerPageDropDown: this.renderSizePageDropdown,
+//     };
+//
+//     return (
+//       <div>
+//         <Widget className="widget-p-md">
+//           <p className="headline-2">Users</p>
+//           <div>
+//             <BootstrapTable bordered={false} data={rows} version="4" pagination options={options} search className="table-responsive pagination-borderless" tableContainerClass={`table-responsive table-striped ${s.usersListTableMobile}`}>
+//               <TableHeaderColumn dataField="avatars" dataSort dataFormat={dataFormat.imageFormatter}>
+//                 <span className="fs-sm">Avatar</span>
+//               </TableHeaderColumn>
+//               <TableHeaderColumn dataField="firstName" dataSort>
+//                 <span className="fs-sm">First Name</span>
+//               </TableHeaderColumn>
+//               <TableHeaderColumn dataField="lastName" dataSort>
+//                 <span className="fs-sm">Last Name</span>
+//               </TableHeaderColumn>
+//               <TableHeaderColumn dataField="phoneNumber" dataSort>
+//                 <span className="fs-sm">Phone Number</span>
+//               </TableHeaderColumn>
+//               <TableHeaderColumn dataField="email" dataSort>
+//                 <span className="fs-sm">E-mail</span>
+//               </TableHeaderColumn>
+//               <TableHeaderColumn dataField="role" dataSort>
+//                 <span className="fs-sm">Role</span>
+//               </TableHeaderColumn>
+//               <TableHeaderColumn dataField="disabled" dataSort dataFormat={dataFormat.booleanFormatter}>
+//                 <span className="fs-sm">Disabled</span>
+//               </TableHeaderColumn>
+//               <TableHeaderColumn isKey dataField="id" dataFormat={this.actionFormatter.bind(this)}>
+//                 <span className="fs-sm">Actions</span>
+//               </TableHeaderColumn>
+//             </BootstrapTable>
+//           </div>
+//         </Widget>
+//
+//         <Modal size="sm" isOpen={this.props.modalOpen} toggle={() => this.closeModal()}>
+//           <ModalHeader toggle={() => this.closeModal()}>Confirm delete</ModalHeader>
+//           <ModalBody>
+//             Are you sure you want to delete this item?
+//           </ModalBody>
+//           <ModalFooter>
+//             <Button color="secondary" onClick={() => this.closeModal()}>Cancel</Button>
+//             <Button color="primary" onClick={() => this.handleDelete()}>Delete</Button>
+//           </ModalFooter>
+//         </Modal>
+//       </div>
+//     )
+//   }
+// }
 
-  openModal(cell) {
-    const userId = cell;
-    this.props.dispatch(actions.doOpenConfirm(userId));
-  }
+const UsersListTable = (props) => {
+  // const [modalOpen, setModalOpen] = useState(false)
+  // const [idToDelete, setIdToDelete] = useState(null)
 
-  closeModal() {
-    this.props.dispatch(actions.doCloseConfirm());
+  const handleDelete = () => {
+    const id = props.idToDelete
+    props.dispatch(actions.doDelete(id))
   }
-
-  actionFormatter(cell) {
+  const openModal = (event, cell) => {
+    const id = cell
+    event.stopPropagation()
+    props.dispatch(actions.doOpenConfirm(id))
+  }
+  const closeModal = () => {
+    props.dispatch(actions.doCloseConfirm())
+  }
+  const actionFormatter = (cell) => {
     return (
       <div className={`d-flex justify-content-between`}>
         <Button
           className={s.controlBtn}
           color="info"
           size="xs"
-          onClick={() => this.props.dispatch(push(`/admin/users/${cell}`))}
+          onClick={() => props.dispatch(push(`/admin/users/${cell}`))}
         >
           View
         </Button>
@@ -61,7 +198,7 @@ class UsersListTable extends Component {
           className={`${s.controlBtn} mx-2`}
           color="success"
           size="xs"
-          onClick={() => this.props.dispatch(push(`/admin/users/${cell}/edit`))}
+          onClick={() => props.dispatch(push(`/admin/users/${cell}/edit`))}
         >
           Edit
         </Button>
@@ -69,111 +206,98 @@ class UsersListTable extends Component {
           className={s.controlBtn}
           color="danger"
           size="xs"
-          onClick={() => this.openModal(cell)}
+          onClick={() => openModal(cell)}
         >
           Delete
         </Button>
       </div>
     )
   }
+  useEffect(() => {
+    console.log("COUNTER")
+    props.dispatch(actions.doFetch({}))
+  }, [])
 
-  componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch(actions.doFetch({}));
-  }
+  const { rows } = props;
+  const columns = [
+    {
+      dataField: "avatar",
+      sort: true,
+      text: "Avatar",
+    },
+    {
+      dataField: "firstName",
+      sort: true,
+      text: "First Name",
+    },
+    {
+      dataField: "lastName",
+      sort: true,
+      text: "Last Name",
+    },
+    {
+      dataField: "phoneNumber",
+      sort: true,
+      text: "Phone Number",
+    },
+    {
+      dataField: "email",
+      sort: true,
+      text: "E-mail",
+    },
+    {
+      dataField: "role",
+      sort: true,
+      text: "Role",
+    },
+    {
+      dataField: "disabled",
+      sort: true,
+      formatter: dataFormat.booleanFormatter,
+      text: "Disabled",
+    },
+    {
+      dataField: "id",
+      formatter: actionFormatter,
+      text: "Actions",
+    },
+  ];
 
-  renderSizePageDropdown = (props) => {
-    const limits = [];
-    props.sizePerPageList.forEach((limit) => {
-      limits.push(<DropdownItem key={limit} onClick={() => props.changeSizePerPage(limit)}>{ limit }</DropdownItem>)
-    });
-
-    return (
-      <ButtonDropdown isOpen={props.open} toggle={props.toggleDropDown}>
-        <DropdownToggle caret>
-          { props.currSizePerPage }
-        </DropdownToggle>
-        <DropdownMenu>
-          { limits }
-        </DropdownMenu>
-      </ButtonDropdown>
-    );
-  };
-
-  createCustomSearchField = (props) => {
-    return (
-      <SearchField
-        className="mb-sm-5 mr-sm-0"
-        placeholder="Search"
-      />
-    );
-  }
-
-  render() {
-    const { rows } = this.props;
-
-    const options = {
-      sizePerPage: 10,
-      paginationSize: 5,
-      searchField: this.createCustomSearchField,
-      sizePerPageDropDown: this.renderSizePageDropdown,
-    };
-
-    return (
-      <div>
-        <Widget className="widget-p-md">
-          <p className="headline-2">Users</p>
-          <div>
-            <BootstrapTable bordered={false} data={rows} version="4" pagination options={options} search className="table-responsive pagination-borderless" tableContainerClass={`table-responsive table-striped ${s.usersListTableMobile}`}>
-              <TableHeaderColumn dataField="avatars" dataSort dataFormat={dataFormat.imageFormatter}>
-                <span className="fs-sm">Avatar</span>
-              </TableHeaderColumn>
-
-              <TableHeaderColumn dataField="firstName" dataSort>
-                <span className="fs-sm">First Name</span>
-              </TableHeaderColumn>
-
-              <TableHeaderColumn dataField="lastName" dataSort>
-                <span className="fs-sm">Last Name</span>
-              </TableHeaderColumn>
-
-              <TableHeaderColumn dataField="phoneNumber" dataSort>
-                <span className="fs-sm">Phone Number</span>
-              </TableHeaderColumn>
-
-              <TableHeaderColumn dataField="email" dataSort>
-                <span className="fs-sm">E-mail</span>
-              </TableHeaderColumn>
-
-              <TableHeaderColumn dataField="role" dataSort>
-                <span className="fs-sm">Role</span>
-              </TableHeaderColumn>
-
-              <TableHeaderColumn dataField="disabled" dataSort dataFormat={dataFormat.booleanFormatter}>
-                <span className="fs-sm">Disabled</span>
-              </TableHeaderColumn>
-
-              <TableHeaderColumn isKey dataField="id" dataFormat={this.actionFormatter.bind(this)}>
-                <span className="fs-sm">Actions</span>
-              </TableHeaderColumn>
-            </BootstrapTable>
-          </div>
-        </Widget>
-
-        <Modal size="sm" isOpen={this.props.modalOpen} toggle={() => this.closeModal()}>
-          <ModalHeader toggle={() => this.closeModal()}>Confirm delete</ModalHeader>
-          <ModalBody>
-            Are you sure you want to delete this item?
-          </ModalBody>
-          <ModalFooter>
-            <Button color="secondary" onClick={() => this.closeModal()}>Cancel</Button>
-            <Button color="primary" onClick={() => this.handleDelete()}>Delete</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-
-    )
-  }
+  return (
+    <div>
+      <Widget className="widget-p-md">
+        <p className="headline-2">Users</p>
+        <ToolkitProvider
+          columns={columns}
+          data={rows}
+          keyField="id"
+          search
+        >
+          { props => (
+            <React.Fragment>
+              <SearchBar className="react-bootstrap-table-search-input" { ...props.searchProps } />
+              <BootstrapTable
+                bordered={false}
+                classes={`table-responsive table-striped ${s.usersListTableMobile}`}
+                pagination={paginationFactory()}
+                { ...props.baseProps }
+              />
+            </React.Fragment>
+          )}
+        </ToolkitProvider>
+      </Widget>
+      <Modal size="sm" isOpen={props.modalOpen} toggle={() => closeModal()}>
+        <ModalHeader toggle={() => closeModal()}>Confirm delete</ModalHeader>
+        <ModalBody>
+          Are you sure you want to delete this item?
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={() => closeModal()}>Cancel</Button>
+          <Button color="primary" onClick={() => handleDelete()}>Delete</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  )
 }
 
 function mapStateToProps(store) {
